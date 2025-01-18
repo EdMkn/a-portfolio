47e21fdedb1ec3bd0558bdb52a57ac4e514b12c1
Manyim Ma Makon <nmaknyim@gmail.com>
2025-01-18 16:39:58 +0100

    Modification profil

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Manyim's Portfolio",
+  description: "My Portfolio made in JS",
 };
 
 export default function RootLayout({
--- data/index.ts
+++ data/index.ts
@@ -173,32 +173,18 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "IT Consultant Intern",
+    desc: "- Optimization of the ICN plugin packaging in order to improve performance and simplify deployment. \n - Participation in the development of an internal document management platform: Factorisation of the code of one of its components to facilitate maintainability.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Javascript Developer Intern",
+    desc: "Collection and manipulation of the data retrieved from multiple devices of the Carrefour market. Automation of certain tasks with scripts made in Javascript. \nThe work Environnment is Google workspace.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
-  },
+  }
 ];
 
 export const socialMedia = [
